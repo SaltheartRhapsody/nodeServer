180a9094bf699caf4772f58dee43f0bc09119751
Zebulon Perrin <saltheartrhapsody@gmail.com>
2014-01-26 12:46:39 -0600

    index file and made server.js into a module

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
+/*******************************************************************
+* Dependencies 
+********************************************************************/
+
 var http = require("http");
 var util = require("util");
+var moment = require("moment");
 
-var requestNumber = 0;
 
+/*******************************************************************
+* Helpers 
+********************************************************************/
 
-http.createServer(function(request, response) {
+var onRequest = function(request, response) {
+
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World");
 	response.end();
 
 	log();
+};
 
-}).listen(8888);
-
-
+var requestNumber = 0;
 var log = function() {
 	requestNumber++;
 	var message = util.format('%d requests have been made\n', requestNumber);
 	process.stdout.write(message);
 };
+
+
+/*******************************************************************
+* Exports
+********************************************************************/
+
+function start() {
+	http.createServer(onRequest).listen(8888);
+	console.log("Server started at: " + moment().format());	
+}
+
+exports.start = start;
+
